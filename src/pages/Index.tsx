2997bdffd25915c9c5f1cafeff0967fd10857e2b
Guillem Ribes Espurz <gribes03@gmail.com>
2025-09-06 22:45:14 +0200

    Included Social Media section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,11 @@
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import ServicesSection from "@/components/ServicesSection";
 import ApproachSection from "@/components/ApproachSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import SocialMediaSection from "./components/SocialMediaSection";
 
 const Index = () => {
   return (
@@ -16,6 +17,7 @@ const Index = () => {
         <ServicesSection />
         <ApproachSection />
         <ContactSection />
+        <SocialMediaSection />
       </main>
       <Footer />
     </div>
